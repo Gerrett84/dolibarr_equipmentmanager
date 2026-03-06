fd099524a795f1e06a32eff66e52a1992b8a9239
Lenin Rivas <53640168+leninrivas@users.noreply.github.com>
2026-03-06 09:07:57 -0500

    Add col for Fix

--- sql/llx_equipmentmanager_v4.6.sql
+++ sql/llx_equipmentmanager_v4.6.sql
@@ -7,3 +7,9 @@
 
 ALTER TABLE llx_equipmentmanager_equipment
     ADD COLUMN IF NOT EXISTS maintenance_month INT DEFAULT NULL COMMENT 'Maintenance Month';
+
+ALTER TABLE llx_equipmentmanager_equipment
+    ADD COLUMN IF NOT EXISTS last_maintenance_date DATE DEFAULT NULL COMMENT 'Last maintenance date';
+
+ALTER TABLE llx_equipmentmanager_equipment
+    ADD COLUMN IF NOT EXISTS next_maintenance_date DATE DEFAULT NULL COMMENT 'Nex maintenance date';
